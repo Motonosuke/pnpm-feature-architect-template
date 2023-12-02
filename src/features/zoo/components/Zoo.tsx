d6e71e25d4699574c9cade4c7566f6d58085422f
Motonosuke <67644458+Motonosuke@users.noreply.github.com>
2023-12-02 22:55:09 +0900

    feat: add zoo

---
+++ src/features/zoo/components/Zoo.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+export const Zoo: FC = () => {
+  return (
+    <div>
+      <h1>Zoo</h1>
+    </div>
+  );
+};
---
+++ src/features/zoo/index.ts
@@ -0,0 +1,1 @@
+export { Zoo } from './components/Zoo';
